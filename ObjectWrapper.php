6f713f0bc9c7d956e27bcad068237c9856e8beaa
Milan Matejcek <milan.matejcek@gmail.com>
2012-10-12 13:12:56 +0200

    add composer

---
+++ ObjectWrapper.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace h4kuna;
+
+use Nette\Object;
+
+/**
+ * @author Milan Matějček
+ */
+abstract class ObjectWrapper extends Object {
+
+    const DS = DIRECTORY_SEPARATOR;
+
+    /** @var resource */
+    protected $resource;
+
+    /** @var string prefix of function */
+    protected $prefix;
+
+    public function __call($name, $args) {
+        $fname = $this->prefix . $name;
+        if (function_exists($fname)) {
+            array_unshift($args, $this->resource);
+            return call_user_func_array($fname, $args);
+        }
+        throw new \RuntimeException('Call undefined method ' . __CLASS__ . '::' . $name);
+    }
+
+    public function getResource() {
+        return $this->resource;
+    }
+
+    public function clearResource() {
+        if (!is_resource($this->resource)) {
+            $this->resource = NULL;
+            return;
+        }
+
+        $this->close(); //magic call or implement
+        $this->resource = NULL;
+    }
+
+    public function __destruct() {
+        $this->clearResource();
+    }
+
+}

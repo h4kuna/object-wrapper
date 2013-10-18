bfb1bb858e45718bf6846f46a57bc48adc119f8e
Milan Matejcek <milan.matejcek@gmail.com>
2013-10-18 12:06:34 +0200

    Curl: send file

--- CUrl.php
+++ CUrl.php
@@ -2,7 +2,7 @@
 
 namespace h4kuna;
 
-use Nette\Utils\Strings;
+use Nette\Utils\MimeTypeDetector;
 
 /**
  * @author Milan Matějček <milan.matejcek@gmail.com>
@@ -136,39 +136,57 @@ class CUrl extends ObjectWrapper {
      * $content = array(
      *            'foo' => 'bar',
      *            'file' => array(
-     *                  'content' => 'file content is simple text', // or path
-     *                  'name' => 'filename.txt',
-     *                  'type' => 'text/plain'
+     *                  'content' => 'file content is simple text', // or path, mantatory
+     *                  'name' => 'filename.txt', // optional
+     *                  'type' => 'text/plain' // optional
      *            ));
      *
      * @param string $url
      * @param array $content
-     * @return CUrl
+     * @return CUrl - call ->exec()
      */
     static function postUploadFile($url, array $content) {
-        $nl = "\r\n";
-        $boundary = '--------CurlBoundary' . Strings::random(18);
+        $eol = "\r\n";
+        $boundary = md5(microtime(TRUE));
         $curl = new static($url, array(
             CURLOPT_SSL_VERIFYPEER => false,
             CURLOPT_SSL_VERIFYHOST => 1,
             CURLOPT_RETURNTRANSFER => 1,
             CURLOPT_HEADER => 0,
             CURLOPT_POST => 1,
-            CURLOPT_VERBOSE => 0,
-            CURLOPT_HTTPHEADER => array('Content-Type: multipart/form-data;charset=utf-8;boundary="' . $boundary . '"'))
+            CURLOPT_VERBOSE => 1,
+            CURLOPT_HTTPHEADER => array('Content-Type: multipart/form-data; charset=utf-8; boundary="' . $boundary . '"'))
         );
         $body = '';
         foreach ($content as $name => $value) {
-            $body .= '--' . $boundary . $nl . 'Content-Disposition: form-data;name="' . $name . '"';
+            $body .= '--' . $boundary . $eol;
+            $body .= 'Content-Disposition: form-data;name="' . $name . '"';
             if (is_array($value)) {
+
+                if (file_exists($value['content'])) {
+                    $type = MimeTypeDetector::fromFile($value['content']);
+                    $content = file_get_contents($value['content']);
+                } else {
+                    $type = MimeTypeDetector::fromString($content = $value['content']);
+                }
+
                 // is file
-                $body .= ';filename="' . $value['name'] . '"' . $nl;
-                $body .= 'Content-Type: ' . $value['type'];
-                $value = file_exists($value['content']) ? file_get_contents($value['content']) : $value['content'];
+                $body .= '; filename="' . (isset($value['name']) ? $value['name'] : date('YmdHis')) . '"' . $eol;
+                $body .= 'Content-Type: ' . (isset($value['type']) ? $value['type'] : $type) . $eol;
+
+                if (preg_match('~base64~i', $content)) {
+                    $body .= 'Content-Transfer-Encoding: base64' . $eol;
+                    $content = preg_replace('~^base64~i', '', $content);
+                }
+
+                $body .= $eol;
+                // $body .= chunk_split(base64_encode($content)); // RFC 2045
+                $body .= trim($content) . $eol;
+            } else {
+                $body .= $eol . $eol . $value . $eol;
             }
-            $body .= $nl . $nl . $value . $nl;
         }
-        $body .= "--$boundary--";
+        $body .= "--$boundary--" . $eol . $eol;
 
         $curl->setopt(CURLOPT_POSTFIELDS, $body);
         return $curl;

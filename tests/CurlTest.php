256ed51ea431f0b91096c6b267af279852b2aecd
Milan Matejcek <milan.matejcek@marketvision.cz>
2014-12-24 06:50:39 +0100

    Add new tests.

--- tests/CurlTest.php
+++ tests/CurlTest.php
@@ -2,37 +2,41 @@
 
 namespace Tests;
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/bootstrap.php';
 
 use h4kuna\CUrl\CUrl;
-use h4kuna\CUrl\CurlBuilder;
-use h4kuna\CUrl\CurlMulti;
+use h4kuna\CUrl\CUrlBuilder;
+use h4kuna\CUrl\CUrlMulti;
 use PHPUnit_Framework_TestCase;
 
 /**
  * @author Milan Matějček
  */
-class CurlTest extends PHPUnit_Framework_TestCase {
+class CurlTest extends PHPUnit_Framework_TestCase
+{
 
     const TEST_URL_95 = 'http://www.cnb.cz/cs/financni_trhy/devizovy_trh/kurzy_devizoveho_trhu/denni_kurz.txt?date=30.12.1995';
     const TEST_URL_96 = 'http://www.cnb.cz/cs/financni_trhy/devizovy_trh/kurzy_devizoveho_trhu/denni_kurz.txt?date=30.12.1996';
 
-    public function testCurlDownload() {
-        $data = CurlBuilder::download(self::TEST_URL_95);
+    public function testCurlDownload()
+    {
+        $data = CUrlBuilder::download(self::TEST_URL_95);
         $this->assertSame(file_get_contents(__DIR__ . '/testCurlDownload.txt'), $data);
     }
 
-    public function testCurlMulti() {
-        $a = CurlBuilder::createDownload(self::TEST_URL_95);
+    public function testCurlMulti()
+    {
+        $a = CUrlBuilder::createDownload(self::TEST_URL_95);
         $b = CUrlBuilder::createDownload(self::TEST_URL_96);
 
-        $multi = new CurlMulti();
+        $multi = new CUrlMulti();
         $multi->addHandles(array('1995-12-30' => $a), $b);
         $multi->exec();
         $this->assertEquals(file_get_contents(__DIR__ . '/testCurlMulti.txt'), serialize($multi->getSelect()));
     }
 
-    public function testCreateFile() {
+    public function testCreateFile()
+    {
         $curl = new CUrl(self::TEST_URL_95);
         $file = $curl->fileCreate(__FILE__);
 
@@ -43,7 +47,8 @@ class CurlTest extends PHPUnit_Framework_TestCase {
         }
     }
 
-    public function _testCurlShare() {
+    public function _testCurlShare()
+    {
         // je funkční ale jestli pracuje správně???
         $a = new CUrl('http://example.com/');
         $a->enableShare();
--- tests/FileTest.php
+++ tests/FileTest.php
@@ -2,7 +2,7 @@
 
 namespace Tests;
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/bootstrap.php';
 
 use h4kuna\File;
 use PHPUnit_Framework_TestCase;
@@ -10,12 +10,33 @@ use PHPUnit_Framework_TestCase;
 /**
  * @author Milan Matějček
  */
-class FileTest extends PHPUnit_Framework_TestCase {
+class FileTest extends PHPUnit_Framework_TestCase
+{
 
-    public function testRead() {
+    public function testRead()
+    {
         $fileName = __DIR__ . '/testRead.txt';
         $file = new File($fileName);
         $this->assertSame(file_get_contents($fileName), $file->read());
+    }
+
+    public function testIterator()
+    {
+        $fileName = __DIR__ . '/testRead.txt';
+        $file = new File($fileName);
+        $content = '';
+        foreach ($file as $line) {
+            $content .= $line;
+        }
+        $this->assertSame(file_get_contents($fileName), $content);
+    }
+
+    public function getFileInfo()
+    {
+        $fileName = __DIR__ . '/testRead.txt';
+        $file = new File($fileName);
+        $info = $file->getFileInfo();
+        $this->assertSame(filemtime($fileName), $info->getMTime());
     }
 
 }

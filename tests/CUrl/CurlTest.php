25f2ebb1ccd8a572702bada06d8a6ce6f6de0f87
Milan Matejcek <milan.matejcek@marketvision.cz>
2015-01-03 05:10:18 +0100

    Tests move to subdirectory.

--- tests/CUrl/CurlTest.php
+++ tests/CUrl/CurlTest.php
@@ -2,7 +2,7 @@
 
 namespace Tests;
 
-require_once __DIR__ . '/bootstrap.php';
+require_once __DIR__ . '/../bootstrap.php';
 
 use h4kuna\CUrl\CUrl;
 use h4kuna\CUrl\CUrlBuilder;
@@ -12,7 +12,7 @@ use PHPUnit_Framework_TestCase;
 /**
  * @author Milan Matějček
  */
-class CurlTest extends PHPUnit_Framework_TestCase
+class CUrlTest extends PHPUnit_Framework_TestCase
 {
 
     const TEST_URL_95 = 'http://www.cnb.cz/cs/financni_trhy/devizovy_trh/kurzy_devizoveho_trhu/denni_kurz.txt?date=30.12.1995';
@@ -21,7 +21,7 @@ class CurlTest extends PHPUnit_Framework_TestCase
     public function testCurlDownload()
     {
         $data = CUrlBuilder::download(self::TEST_URL_95);
-        $this->assertSame(file_get_contents(__DIR__ . '/testCurlDownload.txt'), $data);
+        $this->assertSame(file_get_contents(__DIR__ . '/../testCurlDownload.txt'), $data);
     }
 
     public function testCurlMulti()
@@ -32,7 +32,7 @@ class CurlTest extends PHPUnit_Framework_TestCase
         $multi = new CUrlMulti();
         $multi->addHandles(array('1995-12-30' => $a), $b);
         $multi->exec();
-        $this->assertEquals(file_get_contents(__DIR__ . '/testCurlMulti.txt'), serialize($multi->getSelect()));
+        $this->assertEquals(file_get_contents(__DIR__ . '/../testCurlMulti.txt'), serialize($multi->getSelect()));
     }
 
     public function testCreateFile()
